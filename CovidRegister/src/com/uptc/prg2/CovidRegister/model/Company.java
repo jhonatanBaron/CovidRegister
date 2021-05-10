7ab8e5c3eef5c48ee2999a06d6eeee034de0913e
jhonatanBaron <stevesuar595@gmail.com>
2021-05-10 18:30:28 -0500

    Company class created

---
+++ CovidRegister/src/com/uptc/prg2/CovidRegister/model/Company.java
@@ -0,0 +1,14 @@
+package com.uptc.prg2.CovidRegister.model;
+
+public class Company {
+private Report[] reportList;
+private  Employee[] EmployeeList;
+
+
+
+public boolean checkIncome() {
+	//TODO
+	return false;
+}
+
+}
