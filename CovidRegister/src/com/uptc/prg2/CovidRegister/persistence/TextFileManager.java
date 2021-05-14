6e765ab0a3d5dd6819eb7582931d07f4f7988b7a
jhonatanBaron <stevesuar595@gmail.com>
2021-05-14 09:34:10 -0500

    añadiendo el textFileManager

---
+++ CovidRegister/src/com/uptc/prg2/CovidRegister/persistence/TextFileManager.java
@@ -0,0 +1,47 @@
+package com.uptc.prg2.CovidRegister.persistence;
+
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class TextFileManager {
+	
+
+	public ArrayList<String> readFile() throws IOException{
+		String nameFile = "resources/files/in/CovidMessage.txt";
+		ArrayList<String> linesRead = new ArrayList<>();
+		FileReader fileReader  = new FileReader( nameFile );
+		BufferedReader bufferedReader = new BufferedReader( fileReader ); 
+		String lineRead;
+	 	while( (lineRead = bufferedReader.readLine() ) != null ){
+	 		linesRead.add( lineRead );
+	 	}
+	 	bufferedReader.close(); 
+		return linesRead;
+	}
+	
+	public void writeFile( String nameFile, ArrayList<String> datas ) {
+		FileWriter fileWriter ;
+		try {
+			fileWriter = new FileWriter( nameFile );
+			for (String line : datas) {
+				fileWriter.write( line +  "\n" );
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				fileWriter= new FileWriter(nameFile);
+				fileWriter.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	
+}
